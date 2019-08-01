6af558d902b5c0921dd191e3e158d0828114b205
BoHao.Chen <bohao.chen@wegoluck.com>
2019-08-01 14:13:35 +0800

    新增專案

---
+++ DoubleDispatch/Program.cs
@@ -0,0 +1,78 @@
+﻿// 參考來源
+//https://www.cnblogs.com/loveis715/p/4423464.html
+using System;
+
+namespace DoubleDispatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Single Dispatch
+            Vehicle vehicle = new Vehicle();
+            Vehicle benz = new Benz();
+
+            Sales sales = new Sales();
+            Console.WriteLine("Single Dispatch Demo:");
+            Console.WriteLine($"Sales: The rate for common vehicle is: {sales.GetDiscountRate(vehicle)}");
+            Console.WriteLine($"Sales: The rate for benz is: {sales.GetDiscountRate(benz)}");
+
+
+            // Double Dispatch (fail)
+            SalesManager salesManager = new SalesManager();
+            Console.WriteLine("Double Dispatch Demo(失敗的例子):");
+            Console.WriteLine($"SalesManager: The rate for common vehicle is: {salesManager.GetDiscountRate(vehicle)}");
+            Console.WriteLine($"SalesManager: The rate for benz is: {salesManager.GetDiscountRate(benz)}");
+
+            // Double Dispatch (success)
+            SalesManager salesManager2 = new SalesManager();
+            Console.WriteLine("Double Dispatch Demo(成功的例子):");
+            Console.WriteLine($"SalesManager: The rate for common vehicle is: {salesManager2.GetDiscountRate((dynamic)vehicle)}");
+            Console.WriteLine($"SalesManager: The rate for benz is: {salesManager2.GetDiscountRate((dynamic)benz)}");
+
+            VisitorPattern.Visitor.Run(args);
+
+        }
+    }
+
+    public class Vehicle
+    {
+        public virtual double GetBaseDiscountRate()
+        {
+            return 0.03;
+        }
+    }
+
+    public class Benz : Vehicle
+    {
+        public override double GetBaseDiscountRate()
+        {
+            return 0.06;
+        }
+    }
+
+    public class Sales
+    {
+        public virtual double GetDiscountRate(Vehicle vehicle)
+        {
+            return vehicle.GetBaseDiscountRate();
+        }
+        public virtual double GetDiscountRate(Benz benz)
+        {
+            return benz.GetBaseDiscountRate();
+        }
+    }
+
+    public class SalesManager : Sales
+    {
+        public override double GetDiscountRate(Vehicle vehicle)
+        {
+            return vehicle.GetBaseDiscountRate();
+        }
+        public override double GetDiscountRate(Benz benz)
+        {
+            return benz.GetBaseDiscountRate() * 1.1;
+        }
+    }
+
+}
---
+++ DoubleDispatch/VisitorPattern/Visitor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace DoubleDispatch.VisitorPattern
+{
+    class Visitor
+    {
+        public static void Run(string[] args)
+        {
+            // Single Dispatch
+            Vehicle vehicle = new Vehicle();
+            Vehicle benz = new Benz();
+
+            Sales sales = new Sales();
+            Console.WriteLine("Visitor Single Dispatch Demo:");
+            Console.WriteLine($"Sales: The rate for common vehicle is: {sales.GetDiscountRate(vehicle)}");
+            Console.WriteLine($"Sales: The rate for benz is: {sales.GetDiscountRate(benz)}");
+
+            // Double Dispatch (fail)
+            SalesManager salesManager = new SalesManager();
+            Console.WriteLine("Visitor Double Dispatch Demo(失敗的例子):");
+            Console.WriteLine($"SalesManager: The rate for common vehicle is: {salesManager.GetDiscountRate(vehicle)}");
+            Console.WriteLine($"SalesManager: The rate for benz is: {salesManager.GetDiscountRate(benz)}");
+        }
+    }
+
+
+    public class BaseVehicle // Visitor
+    {
+        public virtual double GetBaseDisountRate(Sales sales) { return 0; }
+        public virtual double GetBaseDisountRate(SalesManager sales) { return 0; }
+    };
+
+    public class Vehicle : BaseVehicle // ConcreteVisitor
+    {
+        public override double GetBaseDisountRate(Sales sales) { return 0.03; }
+        public override double GetBaseDisountRate(SalesManager sales) { return 0.03; }
+    }
+
+    public class Benz : Vehicle // ConcreteVisitor
+    {
+        public override double GetBaseDisountRate(Sales sales) { return 0.06; }
+        public override double GetBaseDisountRate(SalesManager sales) { return 0.06 * 1.1; }
+    }
+
+    public class BaseSales
+    {
+        public virtual double GetDiscountRate(BaseVehicle vehicle) { return 0; }
+    }
+
+    public class Sales : BaseSales
+    {
+        public override double GetDiscountRate(BaseVehicle vehicle) { return vehicle.GetBaseDisountRate(this); }
+    }
+
+    public class SalesManager : BaseSales
+    {
+        public override double GetDiscountRate(BaseVehicle vehicle) { return vehicle.GetBaseDisountRate(this); }
+    }
+
+}
